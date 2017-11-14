b675d190f9e29814d8d55631d909a5f60c41e05b
Daniel Huffman <drhuffman12@yahoo.com>
2017-11-14 01:44:24 -0500

    send json to browser and have the browser pull some data out of the json data, putting the subset of data into a separate web-client output panel

--- src/example_app_kemal.cr
+++ src/example_app_kemal.cr
@@ -28,8 +28,9 @@ ws "/chat" do |socket|
   # Broadcast each message to all clients
   socket.on_message do |message|
     SOCKETS.each { |socket|
-      MESSAGES << message
-      socket.send message
+      msg = { when: Time.now.to_utc, statuses: STATUSES, bot_message: "", human_message: message, socket: socket.to_s }.to_json
+      MESSAGES << msg
+      socket.send msg
     }
   end
 
@@ -39,22 +40,21 @@ ws "/chat" do |socket|
   end
 end
 
-
-  spawn do
-    STATUSES << "running"
-    while STATUSES.last == "running" && ! MESSAGES.last(10).includes?("bot_sleep_mode")
-      begin
-        msg = { when: Time.now.to_utc, statuses: STATUSES, bot_message: (1 + rand(6)).times.to_a.map {|i| CHAR_SET[rand(CHAR_SET_SIZE)] }.join }.to_json
-        puts "bot msg: #{msg}"
-        # SOCKETS.each { |thesocket| thesocket.send("#{Time.now.to_utc}: #{msg}") }
-        SOCKETS.each { |thesocket| thesocket.send(msg) }
-        sleep(10)
-      rescue
-        puts "Socket send error!" # "#{ {dir: __DIR__, file: __FILE__, line: __LINE__, class: self.class.name, method: {{@def.name.stringify}}, message: "some message" }.pretty_inspect }"
-      end
+spawn do
+  STATUSES << "running"
+  while STATUSES.last == "running" && ! MESSAGES.last(10).includes?("bot_sleep_mode")
+    begin
+      msg = { when: Time.now.to_utc, statuses: STATUSES, bot_message: (1 + rand(6)).times.to_a.map {|i| CHAR_SET[rand(CHAR_SET_SIZE)] }.join, human_message: "", socket: "spawn" }.to_json
+      puts "bot msg: #{msg}"
+      # SOCKETS.each { |thesocket| thesocket.send("#{Time.now.to_utc}: #{msg}") }
+      SOCKETS.each { |thesocket| thesocket.send(msg) }
+      sleep(10)
+    rescue
+      puts "Socket send error!" # "#{ {dir: __DIR__, file: __FILE__, line: __LINE__, class: self.class.name, method: {{@def.name.stringify}}, message: "some message" }.pretty_inspect }"
     end
-    STATUSES << "sleeping"
-    SOCKETS.each { |thesocket| thesocket.send({ when: Time.now.to_utc, statuses: STATUSES, message: (1 + rand(6)).times.to_a.map {|i| CHAR_SET[rand(CHAR_SET_SIZE)] }.join }.to_json) }
   end
+  STATUSES << "sleeping"
+  SOCKETS.each { |thesocket| thesocket.send({ when: Time.now.to_utc, statuses: STATUSES, message: (1 + rand(6)).times.to_a.map {|i| CHAR_SET[rand(CHAR_SET_SIZE)] }.join }.to_json) }
+end
 
 Kemal.run
