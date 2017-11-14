a8e551c29105946386c7db4f84199b27a8af3b19
Daniel Huffman <drhuffman12@yahoo.com>
2017-11-13 23:36:07 -0500

    add fake error to show 'where am i' options

--- spec/example_app_kemal_spec.cr
+++ spec/example_app_kemal_spec.cr
@@ -1,9 +1,22 @@
 require "./spec_helper"
 
+class FooException < Exception
+end
+
+class Foo
+  def foo
+    raise FooException.new("#{ {dir: __DIR__, file: __FILE__, line: __LINE__, class: self.class.name, method: {{@def.name.stringify}}, message: "some message" }.pretty_inspect }")
+  end
+end
+
 describe ExampleAppKemal do
   # TODO: Write tests
 
   it "works" do
     false.should eq(true)
+  end
+
+  it "raises an error" do
+    expect_raises(FooException, "bar") { Foo.new.foo }
   end
 end
