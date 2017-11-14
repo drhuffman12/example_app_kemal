57474e28981edfd1c2c90a4f223f44950761afe7
Daniel Huffman <drhuffman12@yahoo.com>
2017-11-14 00:19:48 -0500

    add Websocket '/chat' route and page

--- src/example_app_kemal.cr
+++ src/example_app_kemal.cr
@@ -9,4 +9,27 @@ get "/" do
   "Hello World!"
 end
 
+# chat
+get "/chat" do
+  # TODO: update past Crystal 0.23.1 to get fix for "Error executing run: ecr/process", as noted in "https://github.com/crystal-lang/crystal/issues/4577"
+  render "views/chat/index.ecr"
+end
+
+SOCKETS = [] of HTTP::WebSocket
+
+ws "/chat" do |socket|
+  # Add the client to SOCKETS list
+  SOCKETS << socket
+
+  # Broadcast each message to all clients
+  socket.on_message do |message|
+    SOCKETS.each { |socket| socket.send message}
+  end
+
+  # Remove clients from the list when it's closed
+  socket.on_close do
+    SOCKETS.delete socket
+  end
+end
+
 Kemal.run
