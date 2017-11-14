f0b4d66ae2cac4c633de1551ae4351fb44ce7594
Daniel Huffman <drhuffman12@yahoo.com>
2017-11-13 23:45:11 -0500

    comment out forced error/Logging in spec

--- spec/example_app_kemal_spec.cr
+++ spec/example_app_kemal_spec.cr
@@ -1,13 +1,13 @@
 require "./spec_helper"
 
-class FooException < Exception
-end
+# class FooException < Exception
+# end
 
-class Foo
-  def foo
-    raise FooException.new("#{ {dir: __DIR__, file: __FILE__, line: __LINE__, class: self.class.name, method: {{@def.name.stringify}}, message: "some message" }.pretty_inspect }")
-  end
-end
+# class Foo
+#   def foo
+#     raise FooException.new("#{ {dir: __DIR__, file: __FILE__, line: __LINE__, class: self.class.name, method: {{@def.name.stringify}}, message: "some message" }.pretty_inspect }")
+#   end
+# end
 
 describe ExampleAppKemal do
   # TODO: Write tests
@@ -16,7 +16,7 @@ describe ExampleAppKemal do
     false.should eq(true)
   end
 
-  it "raises an error" do
-    expect_raises(FooException, "bar") { Foo.new.foo }
-  end
+  # it "raises an error" do
+  #   expect_raises(FooException, "bar") { Foo.new.foo }
+  # end
 end
